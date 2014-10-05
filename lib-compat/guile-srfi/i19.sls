e4a02090f5558a3df77d605a20c518624a0d5766
okuoku <mjt@cltn.org>
2014-10-05 23:22:30 +0900

    guile: Add fake SRFI-19

---
+++ lib-compat/guile-srfi/i19.sls
@@ -0,0 +1,8 @@
+(library (guile-srfi i19)
+         (export 
+           time-monotonic
+           time-tai
+           current-time
+           time-second
+           time-nanosecond)   
+         (import (srfi :19)))
--- run/guile.sh
+++ run/guile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec guile -l lib-runtime/guile/guile-load.scm -L lib-r6rs -L lib-stub/guile -L lib-stub/r6rs-common -L lib -L lib-compat $*
+exec guile -l lib-runtime/guile/guile-load.scm -L lib-r6rs -L lib-stub/guile -L lib-stub/r6rs-common -L lib -L lib-compat -L lib-compat/yuni-srfi $*
