36d965b5a7152fefca288b141d0a3827237909d6
okuoku <mjt@cltn.org>
2014-11-03 12:28:24 +0900

    Gauche: Use er-macro-transformer (not working yet)
    Use er-macro-transformer instead of legacy define-macro.
    
    It seems Gauche injects `current-module` reference somewhere.. We must
    find some WAR.

--- lib-runtime/gauche/yuni-runtime/gauche/macro-primitives.scm
+++ lib-runtime/gauche/yuni-runtime/gauche/macro-primitives.scm
@@ -2,29 +2,57 @@
                (export define-inject-syntax))
 
 (select-module yuni-runtime.gauche.macro-primitives)
+(import util.match)
 
-(define (conv lis)
+;; Copied from (chibi-yuni compat macro primitives)
+
+(define-syntax define-inject-syntax
+  (syntax-rules ()
+    ((_ nam syms orig-k)
+     (expand-syms syms orig-k nam))))
+
+(define-syntax expand-syms
+  (er-macro-transformer
+    (lambda (stx here _0)
+      (define (conv lis)
         (map (lambda (s)
                (cond ((symbol? s) (symbol->string s))
                      ((string? s) s)
-                     ((identifier? s) (symbol->string (identifier->symbol s)))
                      (else
                        (error "Invalid object as part of identifier" s))))
              lis))
+      (match stx
+             ((_ (x ...) orig-k nam)
+              (let ((sym (string->symbol (apply string-append (conv x))))
+                    (k (here 'syntax-inject-entry)))
+               `(,k ,sym ,orig-k ,nam)))))))
 
-(define-macro (define-inject-syntax name syms k)
-  (let ((sym-value-value (string->symbol (apply string-append (conv syms))))
-        (expand-syms (gensym))
-        (sym-value (gensym))
-        (b (gensym)))
-    `(begin
-       (define ,sym-value ',sym-value-value)
-       (define-macro ,b
-         (lambda x
-           (case (length x)
-             ((1) (car x))
-             ((2) `(let ((,,sym-value ,(car x))) ,(cadr x)))
-             (else (error "INVALID K ON DEFINE-INJECT-SYNTAX")))))
-       (define-macro (,name param)
-         (list ',k ',b ,sym-value param)))))
+(define (strip sexp)
+  (cond ((pair? sexp)
+         (cons (strip (car sexp))
+               (strip (cdr sexp))))
+        ((vector? sexp)
+         (vector-map strip sexp))
+        ((identifier? sexp)
+         (identifier->symbol sexp))
+        (else sexp)))
+
+(define-syntax syntax-inject-entry
+  (syntax-rules ()
+    ((_ sym k name)
+     (begin
+       (define-syntax b
+         (er-macro-transformer 
+           (lambda (y here _2)
+             (match y
+                    ((_ obj)
+                     (strip obj))
+                    ((_ bind prog)
+                     `(,(here 'letrec)
+                        ((sym ,bind))
+                        ,(strip prog))))))) 
+       (define-syntax name
+         (syntax-rules ()
+           ((_ param)
+            (k b sym param))))))))
 
