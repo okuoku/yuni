920d8b0d9dc511a55c3e00d3f0cfadc5fd0a76bd
okuoku <mjt@cltn.org>
2015-06-12 03:03:30 +0900

    bashism fixes

--- run/nmosh.sh
+++ run/nmosh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-source run/_ostype.sh
+. run/_ostype.sh
 exec nmosh --loadpath=$YUNIMOD:lib-r6rs:lib-stub/r6rs-common:lib:lib-compat:lib-compat/yuni-srfi:lib-stub/nmosh $*
--- run/sash.sh
+++ run/sash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-source run/_ostype.sh
+. run/_ostype.sh
 exec sash --loadpath=$YUNIMOD --loadpath=lib-runtime/r7rs --loadpath=lib-stub/r6rs-common --loadpath=lib-stub/sagittarius --loadpath=lib-compat $*
