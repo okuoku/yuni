849e2641eb91f824605b1d5ac8592f8c4ef15bfc
okuoku <mjt@cltn.org>
2015-06-21 20:54:27 +0900

    guile: Adopt lib-stub/gen library location

--- run/guile.sh
+++ run/guile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . run/_ostype.sh
-exec guile -l lib-runtime/guile/guile-load.scm -L $YUNIMOD -L lib-r6rs -L lib-stub/guile -L lib-stub/r6rs-common -L lib -L lib-compat -L lib-compat/yuni-srfi $*
+exec guile -l lib-runtime/guile/guile-load.scm -L $YUNIMOD -L lib-r6rs -L lib-stub/guile -L lib-stub/r6rs-common -L lib-stub/gen -L lib -L lib-compat -L lib-compat/yuni-srfi $*
