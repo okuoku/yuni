d624ce2d87e7cb6727223b6a3a8d91d5424ff949
okuoku <mjt@cltn.org>
2016-01-11 05:31:06 +0900

    Sagittarius: Adopt command name change

--- run/sash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. run/_ostype.sh
-exec sash --loadpath=$YUNIMOD --loadpath=lib-runtime/r7rs --loadpath=lib-stub/r6rs-common --loadpath=lib-stub/sagittarius --loadpath=lib-stub/gen --loadpath=lib-compat $*
