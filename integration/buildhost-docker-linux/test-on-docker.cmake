81160c17ebbea4fe69be653e85ca806e1066ecd9
okuoku <mjt@cltn.org>
2016-01-31 03:57:13 +0900

    TravisCI: Add .travis.yml using Docker

--- integration/buildhost-docker-linux/test-on-docker.cmake
+++ integration/buildhost-docker-linux/test-on-docker.cmake
@@ -1,6 +1,13 @@
 # Test yuni tree on yunibuild Docker image
+#
+# INPUTs:
+#   IMAGE = image name for doker (defaults to "okuoku/yunibase:testing")
 
 get_filename_component(_myroot ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
+
+if(NOT IMAGE)
+    set(IMAGE "okuoku/yunibase:testing")
+endif()
 
 macro(run_docker_image img rr)
     execute_process(COMMAND
@@ -9,5 +16,9 @@ macro(run_docker_image img rr)
         RESULT_VARIABLE ${rr})
 endmacro()
 
-run_docker_image("okuoku/yunibase:testing" rr)
-run_docker_image("okuoku/yunibase:testing-fedora" rr)
+run_docker_image("${IMAGE}" rr1)
+
+if(rr1)
+    message(FATAL_ERROR "Fail on ${IMAGE}")
+endif()
+
