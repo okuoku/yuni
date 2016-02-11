51b1d57167ae220befa32bc7f4b45e198c9de054
okuoku <mjt@cltn.org>
2016-02-11 16:19:13 +0900

    Kawa: Apply --no-inline to fix some procedures are not called at all

--- run/kawa.sh
+++ run/kawa.sh
@@ -2,6 +2,6 @@
 
 #exec java -Dkawa.import.path="lib-stub/kawa/*.sld" -classpath ./kawa-2.0.jar kawa.repl --r7rs --script lib-runtime/kawa/yuniloader.scm -- $*
 mydir=`pwd`
-exec java -Dkawa.import.path="$mydir/lib-stub/kawa/*.sld" kawa.repl --r7rs --full-tailcalls --script lib-runtime/kawa/yuniloader.scm -- $*
+exec java -Dkawa.import.path="$mydir/lib-stub/kawa/*.sld" kawa.repl --r7rs --full-tailcalls --no-inline --script lib-runtime/kawa/yuniloader.scm -- $*
 
 # FIXME: It seems we cannot use `:` for path separator in Win32...
