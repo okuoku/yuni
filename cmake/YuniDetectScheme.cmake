c14f59d24e66b59382b5e0b954031fbfb0c2eeec
okuoku <mjt@cltn.org>
2016-10-09 00:10:05 +0900

    Gauche: Detect gauche-package on Win32

--- cmake/YuniDetectScheme.cmake
+++ cmake/YuniDetectScheme.cmake
@@ -86,8 +86,7 @@ detect_scheme(YUNI_GOSH NAMES gosh)
 if(NOT WIN32)
     detect_scheme(YUNI_GAUCHE_PACKAGE NAMES gauche-package)
 else(YUNI_GCC)
-    # 0.9.4 gauche-package does not work yet.
-    # detect_scheme(YUNI_GAUCHE_PACKAGE NAMES gauche-package)
+    detect_scheme(YUNI_GAUCHE_PACKAGE NAMES gauche-package)
 endif()
 
 # Guile
