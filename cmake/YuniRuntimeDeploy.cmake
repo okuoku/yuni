d3dc791a59d5420f197676cb637d5760ba752b9b
okuoku <mjt@cltn.org>
2017-02-19 17:53:01 +0900

    build: Fix Cygwin build by adding PROGRAMS option

--- cmake/YuniRuntimeDeploy.cmake
+++ cmake/YuniRuntimeDeploy.cmake
@@ -3,7 +3,7 @@ include(CMakeParseArguments)
 function(yuni_runtime_deploy)
     set(_opts)
     set(_one)
-    set(_multi TARGETS FILES)
+    set(_multi TARGETS FILES PROGRAMS)
     cmake_parse_arguments(ARG "${_opts}" "${_one}" "${_multi}" ${ARGN})
 
     if(ARG_TARGETS)
@@ -13,6 +13,11 @@ function(yuni_runtime_deploy)
 
     if(ARG_FILES)
         install(FILES ${ARG_FILES}
+            DESTINATION ${YUNI_PLATFORM_LIBDIR})
+    endif()
+
+    if(ARG_PROGRAMS)
+        install(PROGRAMS ${ARG_PROGRAMS}
             DESTINATION ${YUNI_PLATFORM_LIBDIR})
     endif()
 
