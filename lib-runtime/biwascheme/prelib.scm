a8263b743190f500ff3b8854b23148fb470dea3e
okuoku <mjt@cltn.org>
2018-06-23 07:34:27 +0900

    BiwaScheme: Implement file ports

--- lib-runtime/biwascheme/prelib.scm
+++ lib-runtime/biwascheme/prelib.scm
@@ -12,36 +12,37 @@
 (define yuni/stdfs (js-eval "require('fs')"))
 (define yuni/biwascheme (yuni/js-import "biwascheme"))
 (define yuni/biwas/Port (js-ref yuni/biwascheme "Port"))
-(define yuni/biwas/CustomInput (js-ref yuni/biwas/Port "CustomInput"))
-(define yuni/biwas/CustomOutput (js-ref yuni/biwas/Port "CustomOutput"))
-
-(define %define-invoke/async
-  (js-eval
-    (string-append
-      "var x = function(biwas){biwas.define_libfunc(\"yuni/js-invoke/async\",2,null,function(ar){"
-      "var js_obj = ar.shift(); var func_name = ar.shift();"
-      "return new biwas.Pause(function(pause){"
-      "var cb = function(){console.log(arguments); return pause.resume(arguments);};"
-      "ar.push(cb);"
-      "console.log(ar);"
-      "js_obj[func_name].apply(js_obj, ar); }); });};x")))
-
-(js-call %define-invoke/async yuni/biwascheme)
+(define yuni/biwas/YuniFileInput (js-ref yuni/biwas/Port "YuniFileInput"))
+(define yuni/biwas/YuniFileOutput (js-ref yuni/biwas/Port "YuniFileOutput"))
+(define yuni/biwas/YuniBinaryFileInput 
+  (js-ref yuni/biwas/Port "YuniFileBinaryInput"))
+(define yuni/biwas/YuniBinaryFileOutput 
+  (js-ref yuni/biwas/Port "YuniFileBinaryOutput"))
 
 (define (yuni/fs-open fs path flags) ;; => (err . fd)
   (let ((x (yuni/js-invoke/async fs "open" path flags)))
    (write x) (newline)
-   (let* ((v (vector-ref x 0))
-          (err (vector-ref v 0))
-          (fd (vector-ref v 1)))
+   (let* ((err (vector-ref x 0))
+          (fd (and (js-null? err) (vector-ref x 1))))
      (cons err fd))))
 
-#|
-(define (open-input-file file)
-  ;; FIXME: Incomplete
-  (let ((fil (yuni/fs-open yuni/stdfs file "r")))
+(define (yuni/openfile cls mode file)
+  (let ((fil (yuni/fs-open yuni/stdfs file mode)))
     (let ((err (car fil))
           (fd (cdr fil)))
-      (write (cons err fd)) (newline)
-      #f)))
-|#
+      (cond
+        ((js-null? err)
+         ;; Success
+         (js-new cls yuni/stdfs fd))
+        (else
+          (write err) (newline)
+          (error "File open error" file err))))))
+
+(define (open-input-file file) 
+  (yuni/openfile yuni/biwas/YuniFileInput "r" file))
+(define (open-output-file file) 
+  (yuni/openfile yuni/biwas/YuniFileOutput "w+" file))
+(define (open-binary-input-file file)
+  (yuni/openfile yuni/biwas/YuniBinaryFileInput "r" file))
+(define (open-binary-output-file file)
+  (yuni/openfile yuni/biwas/YuniBinaryFileOutput "w+" file))
