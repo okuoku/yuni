f54ef0ec5779f79bae4e739131f283ed8dd283d4
okuoku <mjt@cltn.org>
2019-02-17 18:13:26 +0900

    Chicken5: Add selfboot runtime

---
+++ lib-runtime/selfboot/chicken/selfboot-entry.scm
@@ -0,0 +1,137 @@
+;;
+;; Selfboot entrypoint for chicken
+;; 
+;;  $ csi /path/to/selfboot-entry.scm ....
+;;
+
+(import (r7rs)  ;; for define-library
+        (scheme file) (scheme process-context))
+
+(define (%%extract-program-args args* entrypth)
+  (if (string=? (car args*) entrypth)
+    (cdr args*)
+    (%%extract-program-args (cdr args*) entrypth)))
+
+(define (%%extract-entrypoint-path args*)
+  (define (checkone s)
+    (and (string? s) 
+         (let ((len (string-length s)))
+          (and (< 4 len)
+               (string=? (substring s (- len 4) len) ".scm")
+               s))))
+  (and (pair? args*)
+       (or (checkone (car args*))
+           (%%extract-entrypoint-path (cdr args*)))))
+
+(define (%%pathslashfy pth)
+  (let* ((l (string->list pth))
+         (x (map (lambda (c) (if (char=? #\\ c) #\/ c)) l)))
+    (list->string x)))
+
+(define (%%pathsimplify pth)
+  (define (pathcompose acc l)
+    (if (pair? l)
+      (pathcompose (if (string=? (car l) "")
+                     acc
+                     (string-append acc "/" (car l)))
+                   (cdr l))
+      acc))
+  (define (pathcompose-start acc l)
+    (pathcompose (car l) (cdr l)))
+  (define (pathcomponent acc cur strq)
+    (if (string=? strq "")
+      (if (null? acc)
+        (reverse cur)
+        (reverse (cons (list->string (reverse acc)) cur)))
+      (let ((c (string-ref strq 0))
+            (r (substring strq 1 (string-length strq))))
+        (if (char=? c #\/)
+          (pathcomponent '() (cons (list->string (reverse acc)) cur) r)
+          (pathcomponent (cons c acc) cur r)))))
+  (define (simple cur m q)
+    (if (null? q)
+      (if (null? cur)
+        (reverse (cons m cur))
+        (reverse (cdr cur)))
+      (let ((a (car q))
+            (d (cdr q)))
+        (if (string=? ".." a)
+          (let ((next-cur (if (null? cur)
+                            (list "..")
+                            (cdr cur))))
+            (if (null? d)
+              (reverse next-cur)
+              (simple next-cur (car d) (cdr d))))
+          (simple (cons m cur) a d)))))
+  (define (start-simple cur m q)
+    ;; Protect relative ../../../ sequence at beginning
+    (if (string=? m "..")
+      (start-simple (cons m cur) (car q) (cdr q))
+      (simple cur m q)))
+
+  (let ((r (pathcomponent '() '() pth)))
+   (pathcompose-start "" (start-simple '() (car r) (cdr r)))))
+
+(define (%%locate-yuniroot-fromscmpath scmpath)
+  (write %%selfboot-orig-command-line) (newline)
+  (write %%selfboot-mypath) (newline)
+  (let ((npth (%%pathslashfy scmpath)))
+   (%%pathsimplify (string-append npth "/../../../.."))))
+
+(define %%selfboot-orig-command-line (command-line))
+(define %%selfboot-mypath (%%extract-entrypoint-path %%selfboot-orig-command-line))
+(define %%selfboot-yuniroot (%%locate-yuniroot-fromscmpath %%selfboot-mypath))
+(define %%selfboot-program-args (%%extract-program-args
+                                  %%selfboot-orig-command-line
+                                  %%selfboot-mypath))
+
+(define (%%selfboot-filt-exports x)
+  (let loop ((cur '())
+             (q x))
+    (if (pair? q)
+      (let ((a (car q))
+            (d (cdr q)))
+        (case a
+          ((_ ... => else unquote unquote-splicing)
+           (loop cur d))
+          (else
+            (loop (cons a cur) d))))
+      (reverse cur))))
+
+(define (%%selfboot-loadlib pth libname imports exports)
+  (let ((code (%selfboot-file->sexp-list pth)))
+   (eval `(define-library ,libname
+                          (export ,@(%%selfboot-filt-exports exports))
+                          (import (yuni-runtime r7rs) ,@imports)
+                          (begin ,@code)))))
+
+(define (%%selfboot-load-aliaslib truename alias* export*)
+  (for-each (lambda (libname)
+              (let ((code `(define-library ,libname
+                                    (export ,@(%%selfboot-filt-exports
+                                                export*))
+                                    (import ,truename))))
+                (eval code)))
+            alias*))
+
+(define %%selfboot-impl-type 'chicken)
+(define %%selfboot-core-libs '((scheme base)
+                               (scheme case-lambda)
+                               (scheme cxr)
+                               (scheme file)
+                               (scheme inexact)
+                               (scheme process-context)
+                               (scheme read)
+                               (scheme write)
+                               (scheme eval)
+                               ))
+
+
+(when (string=? %%selfboot-yuniroot "")
+  (set! %%selfboot-yuniroot "."))
+
+(load (string-append %%selfboot-yuniroot "/lib-runtime/r7rs/yuni-runtime/r7rs.sld"))
+(load (string-append %%selfboot-yuniroot "/lib-runtime/selfboot/chicken/selfboot-runtime.scm"))
+(load (string-append %%selfboot-yuniroot "/lib-runtime/selfboot/common/common.scm"))
+(load (string-append %%selfboot-yuniroot "/lib-runtime/selfboot/common/run-program.scm"))
+
--- samples/hellolib/README.md
+++ samples/hellolib/README.md
@@ -47,6 +47,7 @@ repository on somewhere.
 - (ChezScheme) `scheme --program ../../lib-runtime/selfboot/chez/selfboot-entry.sps -LIBPATH . app.sps`
 - `s7yuni ../../lib-runtime/selfboot/s7/selfboot-entry.scm -LIBPATH . app.sps`
 - `racket ../../lib-runtime/selfboot/racket/selfboot-entry.rkt -LIBPATH . app.sps`
+- `csi -script ../../lib-runtime/selfboot/chicken/selfboot-entry.scm -LIBPATH . app.sps`
 
 
 ### Docker images
