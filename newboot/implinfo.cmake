3e45465ddb880c3002b67202515dca6904ff6ca5
okuoku <mjt@cltn.org>
2019-03-04 03:28:24 +0900

    newboot: Split implinfo.cmake

---
+++ newboot/implinfo.cmake
@@ -0,0 +1,14 @@
+set(selfboot_CHIBI_SCHEME "chibi-scheme/selfboot-entry.scm")
+set(selfboot_GOSH "gauche/selfboot-entry.scm")
+set(selfboot_SAGITTARIUS "sagittarius/selfboot-entry.sps")
+set(selfboot_CHEZ_SCHEME "chez/selfboot-entry.sps")
+set(selfboot_S7 "s7/selfboot-entry.scm")
+set(selfboot_RACKET "racket/selfboot-entry.rkt")
+set(selfboot_CHICKEN_CSI "chicken/selfboot-entry.scm")
+set(selfboot_GSI "gambit/selfboot-entry.scm")
+set(selfboot_GUILE "guile/selfboot-entry.sps")
+set(selfboot_BIWASYUNI "biwascheme/selfboot-entry.scm")
+
+set(arg_prog_CHEZ_SCHEME "--program")
+set(arg_prog_CHICKEN_CSI "-script")
+
--- newboot/run.cmake
+++ newboot/run.cmake
@@ -22,28 +22,11 @@ if(NOT YUNI_${IMPL})
     message(FATAL_ERROR "Impl ${IMPL} did not found")
 endif()
 
-set(selfboot_CHIBI_SCHEME "chibi-scheme/selfboot-entry.scm")
-set(selfboot_GOSH "gauche/selfboot-entry.scm")
-set(selfboot_SAGITTARIUS "sagittarius/selfboot-entry.sps")
-set(selfboot_CHEZ_SCHEME "chez/selfboot-entry.sps")
-set(selfboot_S7 "s7/selfboot-entry.scm")
-set(selfboot_RACKET "racket/selfboot-entry.rkt")
-set(selfboot_CHICKEN_CSI "chicken/selfboot-entry.scm")
-set(selfboot_GSI "gambit/selfboot-entry.scm")
-set(selfboot_GUILE "guile/selfboot-entry.sps")
-set(selfboot_BIWASYUNI "biwascheme/selfboot-entry.scm")
-
 if(NOT selfboot_${IMPL})
     message(FATAL_ERROR "Impl ${IMPL} is not configured")
 endif()
 
-if(${IMPL} STREQUAL CHEZ_SCHEME)
-    set(arg_prog "--program")
-elseif(${IMPL} STREQUAL CHICKEN_CSI)
-    set(arg_prog "-script")
-else()
-    set(arg_prog)
-endif()
+set(arg_prog ${arg_prog_${IMPL}})
 
 execute_process(
     COMMAND 
