495f786da136b9930bf750dce8da22aa5cd02d91
okuoku <mjt@cltn.org>
2019-03-10 04:47:25 +0900

    Gambit: Implement let-values

--- lib-runtime/gambit/prelib.scm
+++ lib-runtime/gambit/prelib.scm
@@ -4,6 +4,14 @@
   '((yuni . gambit-yuni)
     (scheme . gambit-compat-scheme)))
 
+(define-macro (let-values cls* . body)
+  (if (pair? cls*)
+    (let ((c (car cls*))
+          (d (cdr cls*)))
+      `(receive ,(car c) ,(cadr c)
+         (let-values ,d ,@body)))
+    `(let () ,@body)))
+
 ;; FIXME: yunifake leftover
 (define-macro (define-primitive-names/yunifake . bogus)
   `(begin))
