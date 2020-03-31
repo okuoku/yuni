1ca6e73d47f70b3f0f45946fb2e5e5e664425896
okuoku <mjt@cltn.org>
2020-04-01 08:16:31 +0900

    config: Reorder impl. order in config.scm

--- config/config.scm
+++ config/config.scm
@@ -14,46 +14,72 @@
         r7c-io
         r7c-yunicore)
   (yunifake yunifake-util)
-  ;; FIXME: Hack. We don't have any convention for generated libraries yet.
-  ;; R7RS
-  (compat-chicken
-    (chicken-yuni => yuni)) 
-  (compat-chibi-scheme
-    (chibi-scheme-yuni => yuni))
-  (compat-gauche
-    (gauche-yuni => yuni))
-  (compat-sagittarius
-    (sagittarius-yuni => yuni))
-  (compat-picrin
-    (picrin-yuni => yuni))
-  (compat-kawa
-    (kawa-yuni => yuni))
   (r7rs-common
     (r7rs-common-yuni => yuni))
-  ;; R6RS
+  (r6rs-common
+    (r6rs-common-yuni => yuni))
   ;;; R7RS-bridge: R7RS library for R6RS implementations
   (r7rs-bridge ;; Some R6RS might not need this tough
     (r7b-yuni => yuni) ;; Yuni scheme standard libraries
     r7b-util)
-  (r7b-r7rs-libs ;; Racket may have incompatible R7RS impl.
+  (r7b-r7rs-libs ;; Racket may have incompatible scheme/base library
     (r7b-impl => scheme))
   (r7b-r7rs-libs/racket
     r7b-impl)
-  (compat-guile
-    (guile-yuni => yuni)
-    (guile-r7b => r7b-compat))
-  (compat-guile3
-    (guile-yuni => yuni)) ;; same as Guile2
-  (compat-racket
-    (racket-yuni => yuni)
-    (racket-r7b => r7b-compat))
+  ;; R6RS+R7RS
+  (compat-digamma
+    (digamma-yuni => yuni))
+  (compat-guile3 ;; Guile3 do not use Yuni's own R7RS layer
+    (guile-yuni => yuni))
+  (compat-sagittarius
+    (sagittarius-yuni => yuni))
+  
+  ;; R7RS
+  (compat-chibi-scheme
+    (chibi-scheme-yuni => yuni))
+  (compat-chicken
+    (chicken-yuni => yuni)) 
+  (compat-cyclone
+    (cyclone-yuni => yuni))
+  (compat-gauche
+    (gauche-yuni => yuni))
+  (compat-kawa
+    (kawa-yuni => yuni))
+
+  ;; R6RS
   (compat-chez
     (chez-yuni => yuni)
     (chez-r7b => r7b-compat)
     (chez-r7b-util => r7b-util))
+  (compat-guile ;; Guile 2.x
+    (guile-yuni => yuni)
+    (guile-r7b => r7b-compat))
   (compat-ironscheme
     (ironscheme-yuni => yuni)
     (ironscheme-r7b => r7b-compat))
+  (compat-racket
+    (racket-yuni => yuni)
+    (racket-r7b => r7b-compat))
+
+  ;; Generic Scheme
+  (compat-biwascheme
+    ;; Must sync. with *yuni/libalias*
+    (biwascheme-yuni => yuni)
+    (biwascheme-compat-scheme => scheme))
+  (compat-gambit
+    (gambit-yuni => yuni)
+    (gambit-compat-scheme => scheme))
+  (compat-mit-scheme
+    (mit-scheme-yuni => yuni)
+    (mit-scheme-compat-scheme => scheme))
+  (compat-s7
+    ;; Must sync. with *yuni/libalias*
+    (s7-yuni => yuni)
+    (s7-scheme => scheme))
+
+  ;; Unsupported
+  (compat-picrin
+    (picrin-yuni => yuni))
   (compat-nmosh
     (nmosh-yuni => yuni)
     (nmosh-r7b => r7b-compat)
@@ -65,28 +91,6 @@
     (vicare-yuni => yuni))
   (compat-rapid-gambit
     (rapid-gambit-yuni => yuni))
-  (r6rs-common
-    (r6rs-common-yuni => yuni))
-  ;; Experimental Gambit R5RS
-  (compat-gambit
-    (gambit-yuni => yuni)
-    (gambit-compat-scheme => scheme))
-  ;; Experimental MIT/GNU Scheme R5RS
-  (compat-mit-scheme
-    (mit-scheme-yuni => yuni)
-    (mit-scheme-compat-scheme => scheme))
-  (compat-s7
-    ;; Must sync. with *yuni/libalias*
-    (s7-yuni => yuni)
-    (s7-scheme => scheme))
-  (compat-biwascheme
-    ;; Must sync. with *yuni/libalias*
-    (biwascheme-yuni => yuni)
-    (biwascheme-compat-scheme => scheme))
-  (compat-digamma
-    (digamma-yuni => yuni))
-  (compat-cyclone
-    (cyclone-yuni => yuni))
   )
 
 (GenRacket
