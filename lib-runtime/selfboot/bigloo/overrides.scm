8382c39d40f6dc213a5813d55bf13c7215f75335
okuoku <mjt@cltn.org>
2020-06-21 12:12:46 +0900

    Bigloo: Implement char=? and friends

--- lib-runtime/selfboot/bigloo/overrides.scm
+++ lib-runtime/selfboot/bigloo/overrides.scm
@@ -1,6 +1,23 @@
+(define-syntax %%yuni/define-compar
+  (syntax-rules ()
+    ((_ comp nam)
+     (begin
+       (define (nam a b . args)
+         (and (comp a b)
+              (or (null? args)
+                  (apply nam a args))))
+       (yuni/base-library-add-var! 'nam 'comp)))))
+
+(%%yuni/define-compar char=? char=?+)
+(%%yuni/define-compar char<=? char<=?+)
+(%%yuni/define-compar char>=? char>=?+)
+(%%yuni/define-compar char<? char<?+)
+(%%yuni/define-compar char>? char>?+)
+
 (define (%%yunierror msg irr)
   (error #f msg irr))
 
+(yuni/base-library-add-var! 'char=?+ 'char=?)
 (yuni/base-library-add-var! '%%yunierror 'error)
 (yuni/base-library-add-var! 'yuni/command-line 'command-line)
 
