7f7a9f5bb9466d9c2c3162355ef90061a784dfae
okuoku <mjt@cltn.org>
2022-02-17 21:23:50 +0900

    test: `iter0` should pass now on IronScheme

--- tests/fails.cmake
+++ tests/fails.cmake
@@ -51,9 +51,6 @@ set(expected_failures
     # https://github.com/okuoku/yuni/issues/118
     fail2-RACKET
 
-    # https://github.com/okuoku/yuni/issues/140
-    iter0-IRON_SCHEME
-
     # Syntax
     miniread0-ICYC
     app-ICYC
